6527c310f3b118b98d3e0d033cf5c2986c62a91d
eric409tan <157448990+eric409tan@users.noreply.github.com>
2024-05-01 15:44:50 +0800

    Add files via upload

---
+++ animation_movement.py
@@ -0,0 +1,70 @@
+import pygame
+pygame.init()
+
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Character Movement")
+
+black = (0,0,0)
+
+sprite_sheet_up = pygame.image.load("character_sheet_up.png")
+sprite_sheet_down = pygame.image.load("character_sheet_down.png")
+sprite_sheet_left = pygame.image.load("character_sheet_left.png")
+sprite_sheet_right = pygame.image.load("character_sheet_right.png")
+
+#define frame
+frames_up = []
+frames_down = []
+frames_left = []
+frames_right = []
+ 
+frame_width = 50
+frame_height = 50
+
+def extract_frames(sheet, frame_list):
+    for i in range(8): 
+        frame = sheet.subsurface((i * frame_width, 0), (frame_width, frame_height))
+        frame_list.append(frame)
+
+extract_frames(sprite_sheet_up, frames_up)
+extract_frames(sprite_sheet_down, frames_down)
+extract_frames(sprite_sheet_left, frames_left)
+extract_frames(sprite_sheet_right, frames_right)
+
+character_width = 50
+character_height = 50
+character_x = (screen_width - character_width) // 2
+character_y = (screen_height - character_height) // 2
+character_speed = 5
+current_frames = frames_down
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_a]:
+        character_x -= character_speed
+        current_frames = frames_left
+    if keys[pygame.K_d]:
+        character_x += character_speed
+        current_frames = frames_right
+    if keys[pygame.K_w]:
+        character_y -= character_speed
+        current_frames = frames_up
+    if keys[pygame.K_s]:
+        character_y += character_speed
+        current_frames = frames_down
+
+    screen.fill(black)
+
+    screen.blit(current_frames[int(pygame.time.get_ticks() / 100) % len(current_frames)], (character_x, character_y))
+
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
